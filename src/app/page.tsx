c82d0b2c40c111c946e85c660b98e100ed64e03f
Guilherme Pellegrini <pellegrini458@gmail.com>
2025-04-11 23:40:41 -0300

    Update @pellegrinidev/piggy-ui to version 1.0.14 and integrate AlertDialog component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import { Button } from "@pellegrinidev/piggy-ui";
+'use client';
+
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger, Button } from "@pellegrinidev/piggy-ui";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="">
         <Button>Teste</Button>
+
+        <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button>Deletar conta</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Você tem certeza absoluta?</AlertDialogTitle>
+            <AlertDialogDescription>
+              Essa ação não pode ser desfeita. Isso excluirá sua conta permanentemente.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={() => console.debug("cancelled")}>Cancelar</AlertDialogCancel>
+            <AlertDialogAction onClick={() => console.debug("confirmed")}>Confirmar</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
